9c22837527fc8eff2c555d7ea2e05538cb77a65f
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-23 20:12:17 +0200

    delete mutations and resolvers

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,17 +1,6 @@
 const { ApolloServer } = require("apollo-server");
-
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
+const fs = require("fs");
+const path = require("path");
 
 let links = [
   {
@@ -23,16 +12,37 @@ let links = [
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a hackerNews clone`,
+    info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id,
-    description,
-    url,
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      console.log("post args", args);
+      let idCount = links.length;
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      console.log("args", args);
+      let linkId = parseInt(args.id);
+      console.log("linkId", linkId);
+      const filteredLinks = links.filter((item) => {
+        item.id != linkId;
+      });
+      return filteredLinks
+    },
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+});
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
